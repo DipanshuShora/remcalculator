93c3806b6a5da74f1ce4f5edd52e4695ea518fd6
DipanshuShora <deepanshushora478@gmail.com>
2018-08-04 23:29:46 +0530

    work

--- js/convert.js
+++ js/convert.js
@@ -1,11 +1,11 @@
-var d_value = document.getElementsByName("default-value")[0].value;
-var p_value = document.getElementsByName("pixel-value")[0].value;
-var convert = document.getElementsByName("convert")[0];
-let rem = null;
-convert_rem = function(){
-    rem = p_value/d_value;
-    document.getElementsByClassName("rem-size")[0].innerHTML=rem;
-};
-convert.addEventListener("click",function () {
-    convert_rem();
+var rem = document.getElementById("rem-value");
+var default_value = document.getElementById("default-value");
+var pixel_value = document.getElementById("pixel-value");
+default_value.addEventListener("input",function () {
+    rem.value = pixel_value.value / default_value.value + " rem";
 });
+
+pixel_value.addEventListener("input",function () {
+   rem.value = pixel_value.value / default_value.value + " rem";
+});
+
