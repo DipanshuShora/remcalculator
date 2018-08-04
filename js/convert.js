0e3763e288f84abf0b7cbfb2a7b2239356be77ff
DipanshuShora <deepanshushora478@gmail.com>
2018-08-05 00:53:26 +0530

    keyup works

--- js/convert.js
+++ js/convert.js
@@ -1,11 +1,19 @@
 var rem = document.getElementById("rem-value");
 var default_value = document.getElementById("default-value");
 var pixel_value = document.getElementById("pixel-value");
-default_value.addEventListener("input",function () {
-    rem.value = pixel_value.value / default_value.value + " rem";
+
+// default_value.addEventListener("input",function () {
+//     rem.value = pixel_value.value / default_value.value + " rem";
+// });
+//
+// pixel_value.addEventListener("input",function () {
+//    rem.value = pixel_value.value / default_value.value + " rem";
+// });
+//
+default_value.addEventListener("keyup",function () {
+    rem.value = pixel_value.value / default_value.value + " Rem";
 });
 
-pixel_value.addEventListener("input",function () {
-   rem.value = pixel_value.value / default_value.value + " rem";
+pixel_value.addEventListener("keyup",function () {
+   rem.value = pixel_value.value / default_value.value + " Rem";
 });
-
